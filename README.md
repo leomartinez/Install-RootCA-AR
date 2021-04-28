d6b0614bbb19bc743c1753258074858748060863
Leonardo Martinez <lmartinez@santafe.gob.ar>
2021-04-28 13:10:01 -0300

    Agregado README.md y esqueleto del script de bash.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Instalación de la Cadena de Certificación de la AC Raíz de Argentina
+
+EL objetivo de este proyecto es ofrecer una herramienta para instalar la *Cadena de Certificación de la AC Raíz de Argentina* [^1] para validar los certificados emitidos por la *Autoridad Certificante de la Administración Pública* [^2] en el sistema operativo *GNU/Linux* basados en *Debian/Ubuntu*.
+
+[^1]: [Autoridad Certificante Raíz de la República Argentina](https://www.argentina.gob.ar/jefatura/innovacion-publica/administrativa/firmadigital/acraiz)
+
+[^2]: [Autoridad Certificante de la Administración Pública](https://www.argentina.gob.ar/jefatura/innovacion-publica/administrativa/firmadigital/acap)
+
+
+## Enlaces
+
+[Descargas y tutoriales](https://www.argentina.gob.ar/modernizacion/firmadigital/descargas-tutoriales)
---
+++ instalaCERTs.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+## ARCHIVO: /usr/local/sbin/instalaCERTs.sh
+##              
+## USO: instalaCERTs.sh
+##
+## GitHub Project:
+## https://github.com/leomartinez/Install-RootCA-AR.git
+##
+
+#############################
+## DEFINICIÓN DE VARIABLES ##
+#############################
+
+# Directorios
+
+
+# Archivos
+
+
+# Comandos
+
+
+# Variables locales
+
+
+# Variables de representación en pantalla
+
+linea="==============================================================================="
+reset_terminal="tput sgr0"
+negrita_ini="\033[1m"
+negrita_fin="\033[0m"
+negro="\E[30m"
+negro_bg="\E[40m"
+rojo="\E[31m"
+rojo_bg="\E[41m"
+verde="\E[32m"
+verde_bg="\E[42m"
+amarillo="\E[33m"
+amarillo_bg="\E[43m"
+azul="\E[34m"
+azul_bg="\E[44m"
+magenta="\E[35m"
+magenta_bg="\E[45m"
+cyan="\E[36m"
+cyan_bg="\E[46m"
+blanco="\E[37m"
+blanco_bg="\E[47m"
+
+
+#############################
+## DEFINICIÓN DE FUNCIONES ##
+#############################
+
+
+#############################
+##    SECCIÓN PRINCIPAL    ##
+#############################
+
+
+#############################
+##    FINAL DEL SCRIPT     ##
+#############################
